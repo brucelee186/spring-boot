0f6ecab4139e9304e19d14dc5c295259d07c2e72
Administrator <Administrator@WIN-OCJQN537GVE>
2019-10-18 11:49:13 +0800

    添加登录测试

--- src/main/java/com/example/demo/controller/BootController.java
+++ src/main/java/com/example/demo/controller/BootController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,16 @@ public class BootController {
 	@RequestMapping("/")
 	String home() {
 		return "Home Sweet Home";
+	}
+	
+	@RequestMapping("/boot")
+	String boot() {
+		return "booting...";
+	}
+	
+	@RequestMapping("/login")
+	String login(@RequestParam String username, @RequestParam String password) {
+		String res = "用户名: " + username + " 密码: " + password;
+		return res;
 	}
 }
